25020f7c223ec377a9722d4f7d07fd628c543d23
DiegoAlessandroMotta <153887376+DiegoAlessandroMotta@users.noreply.github.com>
2025-10-20 18:51:58 -0500

    feat: add listener for deleting expired FCM notification tokens on failure

---
+++ server/app/Listeners/Fcm/DeleteExpiredNotificationTokens.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Listeners\Fcm;
+
+use Arr;
+use Illuminate\Notifications\Events\NotificationFailed;
+use NotificationChannels\Fcm\FcmChannel;
+
+class DeleteExpiredNotificationTokens
+{
+  public function handle(NotificationFailed $event): void
+  {
+    if ($event->channel == FcmChannel::class) {
+
+      $report = Arr::get($event->data, 'report');
+
+      $target = $report->target();
+
+      $event->notifiable->fcmDeviceTokens()
+        ->where('token', $target->value())
+        ->delete();
+    }
+  }
+}
